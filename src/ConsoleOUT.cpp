b60dbe7f63d8527fa918730d1d8628e427cbabc7
KatoMisa <kato@rsdlab.jp>
2024-08-21 14:19:09 +0900

    first commit

---
+++ src/ConsoleOUT.cpp
@@ -0,0 +1,123 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  ConsoleOUT.cpp
+ * @brief ModuleDescription
+ *
+ */
+// </rtc-template>
+
+#include "ConsoleOUT.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+#if RTM_MAJOR_VERSION >= 2
+static const char* const consoleout_spec[] =
+#else
+static const char* consoleout_spec[] =
+#endif
+  {
+    "implementation_id", "ConsoleOUT",
+    "type_name",         "ConsoleOUT",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "VenderName",
+    "category",          "Test",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+ConsoleOUT::ConsoleOUT(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_str_inIn("str_in", m_str_in),
+    m_num_inIn("num_in", m_num_in),
+    m_str_outOut("str_out", m_str_out),
+    m_num_outOut("num_out", m_num_out)
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+ConsoleOUT::~ConsoleOUT()
+{
+}
+
+
+
+RTC::ReturnCode_t ConsoleOUT::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("str_in", m_str_inIn);
+  addInPort("num_in", m_num_inIn);
+  
+  // Set OutPort buffer
+  addOutPort("str_out", m_str_outOut);
+  addOutPort("num_out", m_num_outOut);
+
+  
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleOUT::onActivated(RTC::UniqueId /*ec_id*/)
+{
+  std::cout << "Activate ";
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleOUT::onDeactivated(RTC::UniqueId /*ec_id*/)
+{
+  std::cout << "Deactivate ";
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleOUT::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  if (m_num_inIn.isNew())
+    {
+     m_num_inIn.read();
+     std::cout << "Received: " << m_num_in.data << std::endl;
+   }  
+
+  if (m_str_inIn.isNew())
+    {
+     m_str_inIn.read();
+     std::cout << "Received: " << m_str_in.data << std::endl;
+   }  
+
+
+  return RTC::RTC_OK;
+}
+
+
+
+
+
+extern "C"
+{
+ 
+  void ConsoleOUTInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(consoleout_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ConsoleOUT>,
+                             RTC::Delete<ConsoleOUT>);
+  }
+  
+}
---
+++ test/src/ConsoleOUTTest.cpp
@@ -0,0 +1,171 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  ConsoleOUTTest.cpp
+ * @brief ModuleDescription (test code)
+ *
+ */
+// </rtc-template>
+
+#include "ConsoleOUTTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+#if RTM_MAJOR_VERSION >= 2
+static const char* const consoleout_spec[] =
+#else
+static const char* consoleout_spec[] =
+#endif
+  {
+    "implementation_id", "ConsoleOUTTest",
+    "type_name",         "ConsoleOUTTest",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "VenderName",
+    "category",          "Test",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+ConsoleOUTTest::ConsoleOUTTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_str_inOut("str_in", m_str_in),
+    m_num_inOut("num_in", m_num_in),
+    m_str_outIn("str_out", m_str_out),
+    m_num_outIn("num_out", m_num_out)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+ConsoleOUTTest::~ConsoleOUTTest()
+{
+}
+
+
+
+RTC::ReturnCode_t ConsoleOUTTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("str_out", m_str_outIn);
+  addInPort("num_out", m_num_outIn);
+  
+  // Set OutPort buffer
+  addOutPort("str_in", m_str_inOut);
+  addOutPort("num_in", m_num_inOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ConsoleOUTTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+//RTC::ReturnCode_t ConsoleOUTTest::onStartup(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleOUTTest::onShutdown(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+RTC::ReturnCode_t ConsoleOUTTest::onActivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleOUTTest::onDeactivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleOUTTest::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+//RTC::ReturnCode_t ConsoleOUTTest::onAborting(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleOUTTest::onError(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleOUTTest::onReset(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleOUTTest::onStateUpdate(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t ConsoleOUTTest::onRateChanged(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+bool ConsoleOUTTest::runTest()
+{
+    return true;
+}
+
+
+extern "C"
+{
+ 
+  void ConsoleOUTTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(consoleout_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ConsoleOUTTest>,
+                             RTC::Delete<ConsoleOUTTest>);
+  }
+  
+}
